bb936514242d253b58c307038c85c14c6fb11430
iyeaaa <iyeaaa@icloud.com>
2023-05-02 15:03:31 +0900

    SearchBar 생성, Appbar 생성

--- lib/pages/searchpage.dart
+++ lib/pages/searchpage.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:medicine_app/util/SearchWidget.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -11,12 +12,29 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
+    double baseWidth = 380;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+
     return Scaffold(
       backgroundColor: const Color(0xFFFCFCFC),
-      appBar: AppBar(
-        title: const Text("Search Page"),
+      body: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFA07EFF),
+          elevation: 0,
+          toolbarHeight: 80*fem,
+          // leading: ,
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(30 * fem, 20 * fem, 30 * fem, 20 * fem),
+            child: Column(
+              children: [
+                SearchWidget(fem).make(),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
